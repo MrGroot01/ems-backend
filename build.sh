444bd821356a104001f1f99c16a128a8c905bf8b
Shivasup <kirand09876@gmail.com>
2026-06-06 10:29:46 +0530

    debug: show render database users

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
-#!/usr/bin/env bash 
-set -o errexit 
-pip install -r requirements.txt 
-python manage.py collectstatic --no-input 
-python manage.py migrate 
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
+python manage.py shell -c "
+from users.models import User
+print('=== ALL USERS IN RENDER DATABASE ===')
+for u in User.objects.all():
+    print(f'Email: {u.email} | Name: {u.full_name} | Active: {u.is_active}')
+print('=== END ===')
+"
